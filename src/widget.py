5f0c4dc4c496767e24726b073f8f866ee65cff65
Антон Жилин <neirophoton@gmail.com>
2026-09-12 21:02:38 +0300

    fix bug

--- src/widget.py
+++ src/widget.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         "Visa Classic 6831982476737658",
         "Visa Platinum 8990922113665229",
         "Visa Gold 5999414228426353",
-        "Счет 73654108430135874305"
+        "Счет 73654108430135874305",
     ]
 
     for data in test_data_cards:
